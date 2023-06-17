8a5067eb4b259c52684a3a7a031dfbfb29d835f1
rohit-kain <rohit729verma@gmail.com>
2023-06-18 02:25:52 +0530

    committed by Rahul kumar dev .

--- src/com/app/Main.java
+++ src/com/app/Main.java
@@ -7,6 +7,14 @@ public class Main {
 		int a = 10;
 		int b = 23;
 		add(a, b);
+		multiply(a, b);
+		System.out.println("Hello World");
+		Test2.printMsg("you bro");
+	}
+
+	private static void multiply(int a, int b) {
+		System.out.println(a * b);
+
 	}
 
 	private static void add(int a, int b) {
---
+++ src/com/app/Test2.java
@@ -0,0 +1,8 @@
+package com.app;
+
+public class Test2 {
+
+	public static void printMsg(String str) {
+		System.out.println(str);
+	}
+}
