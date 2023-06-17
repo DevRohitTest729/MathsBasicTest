e6032fb44c387797d7f50b3ebeb20c10c2f08273
rohit-kain <rohit729verma@gmail.com>
2023-06-18 01:57:50 +0530

    project from team leader first commit

---
+++ src/com/app/Main.java
@@ -0,0 +1,17 @@
+package com.app;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int a = 10;
+		int b = 23;
+		add(a, b);
+	}
+
+	private static void add(int a, int b) {
+
+		System.out.println(a + b);
+
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author tough
+ *
+ */
+module BasicMaths {
+}
